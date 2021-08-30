eb85948602410403fc3cb29c80d4065492182b39
yamb144 <yam.bitton@gmail.com>
2021-08-30 22:12:29 +0300

    סופי

--- equation.js
+++ equation.js
@@ -1,5 +1,6 @@
 const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
 let solutionA = solutionB = 0
-solutionA = a*a -2*b +c;
-solutionB =0
-console.log(solutionA, solutionB)
+let root = b*b -4*a*c;
+solutionA = (-b + Math.sqrt(root))/2*a
+solutionB =(-b - Math.sqrt(root))/2*a
+console.log(solutionA,solutionB)
