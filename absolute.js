e7e5fe991358cfafedbd77da69ced8e98e21169f
yamb144 <yam.bitton@gmail.com>
2021-08-30 21:55:25 +0300

    פתרון

---
+++ absolute.js
@@ -0,0 +1,7 @@
+const num = 548
+let absolute;
+if (num>=0)
+absolute=num;
+else 
+absolute = num-num -num;
+console.log(absolute)
---
+++ equation.js
@@ -0,0 +1,5 @@
+const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
+let solutionA = solutionB = 0
+solutionA = a*a -2*b +c;
+solutionB =0
+console.log(solutionA, solutionB)
