28fc782241c5bf9bb0d06be615b5770cfe037b41
yamb144 <yam.bitton@gmail.com>
2021-08-31 04:01:48 +0300

    new

---
+++ grades.js
@@ -0,0 +1,21 @@
+
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+function gradedef(grades){
+
+
+let lowest =grades[0];
+let highest =grades[0];
+// your code goes here
+for (let i =0; i< grades.length ;i++) {
+    if (grades[i]>highest)
+    highest=grades[i];
+    if (grades[i]<lowest)
+    lowest=grades[i];
+}
+console.log(highest- lowest);
+}
+gradedef(grades);
+
+function median(grade) {
+    let med =[]
+}
