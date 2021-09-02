a8962a8ca1b4a972eecbb0399c37b95d9d0c28f2
yamb144 <yam.bitton@gmail.com>
2021-09-02 14:42:32 +0300

    good

---
+++ formula.js
@@ -0,0 +1,38 @@
+
+// list of Formula 1 champions since the year 2000
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {}
+
+function countWin(driver) {
+ if (!(driver in winners)){
+     winners[driver]=0;
+ }
+ winners[driver]++
+ 
+
+}
+
+formula1Champions.forEach(countWin)
+console.log(winners)
